75d1941fd55eb31533ca15c888e646c6da148f1c
ananya <lincolnav829@gmail.com>
2018-08-19 21:47:11 +0530

    adding the accounts file

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+from .forms import SignUpForm
 
 
 def signup(request):
-	return render(request, 'signup.html')
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username,password=raw_password)
+			login(request, user)
+			return redirect('home')
+	else:
+		form = SignUpForm()
+	return render(request, 'signup.html', {'form': form})
